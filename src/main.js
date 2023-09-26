d57d5054ac92202a6eb0a6f70f5aa3b9f0319200
zhifeiji <jjzhifeiji@gmail.com>
2023-09-27 01:02:01 +0800

    config baseURL

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import global_ from './baseUrl.js'
 import qs from 'qs'
 import 'font-awesome/css/font-awesome.min.css'
 import VueClipboard from 'vue-clipboard2'
-import { quillEditor } from "vue-quill-editor"; //调用编辑器
+import {quillEditor} from "vue-quill-editor"; //调用编辑器
 import 'quill/dist/quill.core.css';
 import 'quill/dist/quill.snow.css';
 import 'quill/dist/quill.bubble.css';
@@ -29,33 +29,33 @@ Vue.use(quillEditor);
 Vue.use(VueClipboard)
 VueClipboard.config.autoSetContainer = true
 
+URL = require('../static/config.json');
+axios.defaults.baseURL = URL.apiUrl;
 Vue.prototype.$http = axios;
 Vue.prototype.GLOBAL = global_;
-axios.defaults.baseURL = "https://service.tmaa.cc/";
 
 const router = new VueRouter({
-  // mode: 'history',
-  routes,
+    // mode: 'history',
+    routes,
 });
-
 
 
 // 路由监听是否登录，有无user
 router.beforeEach((to, from, next) => {
-  if (to.path == '/login') {
-    sessionStorage.clear();
-  }
-  let user = JSON.parse(sessionStorage.getItem('token'));
-  if (!user && to.path != '/login') {
-    next({ path: '/login' })
-  } else {
-    next()
-  }
+    if (to.path == '/login') {
+        sessionStorage.clear();
+    }
+    let user = JSON.parse(sessionStorage.getItem('token'));
+    if (!user && to.path != '/login') {
+        next({path: '/login'})
+    } else {
+        next()
+    }
 });
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
 
